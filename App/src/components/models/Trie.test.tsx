f5e842adf3f3c9c2f348f0f6453d5e9084a59603
jdsmith104 <jdsmith104@outlook.com>
2022-05-17 08:18:03 +0100

    Implemented search and added test cases

--- App/src/components/models/Trie.test.tsx
+++ App/src/components/models/Trie.test.tsx
@@ -1,4 +1,4 @@
-import { TrieNode } from './Trie';
+import { toIndex, TrieNode } from './Trie';
 
 test('test case 1', () => {
   const defaultValue: string = '';
@@ -6,4 +6,39 @@ test('test case 1', () => {
   const trieNode = new TrieNode({ val: defaultValue });
 
   expect(trieNode.getValue()).toBe(defaultValue);
+});
+
+test('test case 2', () => {
+  const defaultValue: string = '';
+
+  const head = new TrieNode({ val: defaultValue });
+
+  expect(head.getValue()).toBe(defaultValue);
+});
+
+test('test case 3', () => {
+  const defaultValue: string = '';
+
+  const head = new TrieNode({ val: defaultValue });
+  const opts = { name: 'test' };
+  let response: boolean = head.addQuery('hello', opts);
+  expect(response).toBe(true);
+
+  response = head.addQuery('help', opts);
+  expect(response).toBe(true);
+
+  response = head.addQuery('help me', opts);
+  expect(response).toBe(true);
+
+  response = head.addQuery('help me', opts);
+  expect(response).toBe(false);
+});
+
+test('test toIndex', () => {
+  const letters = ['A', 'a', 'b', ','];
+  const expectedIndex = [true, true, true, false];
+  for (let i = 0; i < letters.length; i += 1) {
+    const response: any = toIndex(letters[i]);
+    expect(response?.success).toBe(expectedIndex[i]);
+  }
 });
--- App/src/components/models/Trie.tsx
+++ App/src/components/models/Trie.tsx
@@ -1,5 +1,25 @@
-export interface TrieParameters {
+import { assert } from 'console';
+
+export interface ITrieParameters {
   val: string;
+}
+
+interface IResponse {
+  success: boolean;
+  val: any;
+}
+
+function toIndex(letter: string): IResponse {
+  assert(letter.length === 1);
+  const response: IResponse = { success: false, val: 0 };
+  const index = letter.toLowerCase().charCodeAt(0) - 97;
+  response.val = index;
+  if (index < 0 || index >= 26) {
+    response.success = false;
+  } else {
+    response.success = true;
+  }
+  return response;
 }
 
 class TrieNode {
@@ -7,15 +27,71 @@ class TrieNode {
 
   val: string;
 
-  constructor(parameters: TrieParameters) {
+  options: any;
+
+  constructor(parameters: ITrieParameters) {
     this.val = parameters.val;
 
     this.children = new Array<TrieNode | undefined>(26);
+
+    this.options = undefined;
   }
 
   getValue(): string {
     return this.val;
   }
+
+  addQuery(query: string, options: any): boolean {
+    const parsedQuery = query.toLowerCase().replaceAll(' ', '');
+    const response = this.add(parsedQuery, options);
+    if (response) {
+      return true;
+    }
+    return false;
+  }
+
+  private add(query: string, options: any): undefined | boolean {
+    if (query.length > 0) {
+      const letter = query[0];
+      const response = toIndex(letter);
+      // If letter has been converted to valid index
+      if (response.success === true) {
+        const index = response.val;
+        if (this.children[index] === undefined) {
+          this.children[index] = new TrieNode({ val: letter });
+        }
+        const nextQuery: string = query.substring(1, query.length);
+        return this.children[index]?.addQuery(nextQuery, options);
+      }
+      return false;
+    }
+
+    // Return false if query already in structure
+    if (this.options) {
+      return false;
+    }
+
+    this.options = options;
+    return true;
+  }
+
+  search(query: string): TrieNode | undefined {
+    if (query.length > 0) {
+      const letter = query[0];
+      const response = toIndex(letter);
+      // If letter has been converted to valid index
+      if (response.success === true) {
+        const index = response.val;
+        if (this.children[index] !== undefined) {
+          return this.children[index]?.search(query.substring(1, query.length));
+        }
+        return undefined;
+      }
+      return undefined;
+    }
+    // Successful path
+    return this;
+  }
 }
 
-export { TrieNode };
+export { TrieNode, toIndex };
